9621842d942a9c4c4c0b57f0259bace9966baaa3
DTGlov <dtglover21@gmail.com>
2022-11-26 16:45:50 +0000

    rename web app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { BiNote } from 'react-icons/bi';
 export default function Header() {
   return (
     <nav className="h-16 m-8 rounded-lg bg-black shadow-lg text-white flex items-center justify-center">
-      <h1 className="text-center text-white font-bold text-3xl">NoteTaker</h1>
+      <h1 className="text-center text-white font-bold text-3xl">NoteTracker</h1>
       <BiNote size={30} />
     </nav>
   );
